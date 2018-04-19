640f0d8e3fe6dcd6b6d98ff565f7029bea9b221d
Friedrike Preu <friedrike.preu@codecentric.de>
2018-04-19 10:05:14 +0200

    initial commit - placeholder files

---
+++ compile.R
@@ -0,0 +1,9 @@
+# setwd('~/Git/projekthandbuch')
+
+bookdown::render_book("01-index.Rmd", output_dir = "docs")
+# gitbook
+
+# PDF
+# bookdown::render_book("index.Rmd", output_dir = "docs",
+# 	output_format = bookdown::pdf_book(),
+# 	params = list(emoji = FALSE))
